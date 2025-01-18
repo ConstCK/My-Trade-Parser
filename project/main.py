7073ef7eb2498018af10a6c9314be2fa04165e54
Constantin <Constantin_CK@outlook.com>
2025-01-18 15:52:11 +0300

    refactor: Refactor main file

--- project/main.py
+++ project/main.py
@@ -19,25 +19,29 @@ async def main():
     urls = await my_parser.get_xls_urls(2024)
     # Получение всех данных асинхронно
     start = time.time()
-    tasks = [asyncio.create_task(my_parser.get_xls_data(url)) for url in urls]
+    tasks = [my_parser.get_xls_data(url) for url in urls]
     await asyncio.gather(*tasks)
-    print(f'Async data fetching time is {time.time() - start} seconds')
+    async_data_fetching = f'Async data fetching time is {time.time() - start} seconds'
     # Получение всех данных синхронно
     start = time.time()
     for url in urls:
         await my_parser.get_xls_data(url)
-    print(f'Sync data fetching time is {time.time() - start} seconds')
+    sync_data_fetching = f'Sync data fetching time is {time.time() - start} seconds'
     # Извлечение всех данных для добавления в БД
     result = await my_parser.get_all_data()
     # Добавление данных в БД используя асинхронную сессию
     await db_service.async_create_or_update_data(result)
     # Добавление данных в БД используя синхронную сессию
     await db_service.sync_create_or_update_data(result)
+    print(sync_data_fetching)
+    print(async_data_fetching)
 
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Finishing program execution...')
     except Exception as error:
         print(
             f'There are some app problems...exiting program...Error description: {error}')
